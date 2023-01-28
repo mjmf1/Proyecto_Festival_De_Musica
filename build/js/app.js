cfd02b9d2e4774c160adc0a8daa1c21968896363
mjmf1 <marcomora61@gmail.com>
2023-01-28 10:15:41 -0500

    Generando el HTML de la galería

--- build/js/app.js
+++ build/js/app.js
@@ -9,5 +9,16 @@ function iniciciarApp(params) {
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
 
-    galeria.textContent = 'crear galeria'
+    for (let i = 1; i <= 12; i++) {
+       const imagen = document.createElement('picture');
+
+       imagen.innerHTML = `
+       <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+       <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+       <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+   `;
+
+       galeria.appendChild(imagen);
+        
+    }
 }
