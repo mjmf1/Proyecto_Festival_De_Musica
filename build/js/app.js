b4358c483a00dca49dac97041f579a98c79d6e85
mjmf1 <marcomora61@gmail.com>
2023-01-30 19:49:06 -0500

    Detectando la imagen a la que le damos click para mostrar una versión más grande

--- build/js/app.js
+++ build/js/app.js
@@ -17,8 +17,28 @@ function crearGaleria() {
        <source srcset="build/img/thumb/${i}.webp" type="image/webp">
        <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
    `;
+   imagen.onclick = function name() {
+        mostarImagen(i);
+   }
 
        galeria.appendChild(imagen);
         
     }
+}
+
+function mostarImagen(id) {
+    const imagen = document.createElement('picture');
+
+    imagen.innerHTML = `
+    <source srcset="build/img/grande/${id}.avif" type="image/avif">
+    <source srcset="build/img/grande/${id}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+`;
+
+const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
 }
