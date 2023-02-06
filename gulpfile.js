d6a07d9c6ca92c4fb0d949eef9caa54999475928
mjmf1 <marcomora61@gmail.com>
2023-02-05 21:28:22 -0500

    Mejorar el performance del código con Gulp y CSS Nano

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ const {src, dest , watch, parallel} = require("gulp");
 // CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
 
 // Imagenes
 const cache = require('gulp-cache');
@@ -15,6 +18,7 @@ function css(done) {
     src("src/scss/**/*.scss")// Identificar el archivo de SASS
     .pipe(plumber())
     .pipe(sass())//Compilarlo
+    .pipe(postcss([autoprefixer(), cssnano() ]) )
     .pipe(dest("build/css"))//Almancenarlo en el disco duro
 
     done();
